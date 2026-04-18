c7a6aac8588b7297964beab64b1eb1376442673f
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-04-18 13:16:52 -0300

    factory functions

--- factory-function/factory.js
+++ factory-function/factory.js
@@ -1,30 +1,42 @@
-//Factory function (Função fábrica)
-//Construtor function
-function criaPessoa(nome, sobrenome, alt, peso){
-    return{
-        nome,
-        sobrenome,
-        get nomeCompleto(){
-            return `${this.nome} ${this.sobrenome}`
-        },
-        set nomeCompleto(valor){
-            valor = valor.split(' ')//Divide uma string separando e retornando como um array
-            this.nome = valor.shift()
-            this.sobrenome = valor.join(' ')
-            console.log(valor)
-        },
-        fala(assunto){
-            return `${this.nome} está ${this.assunto}.`
-        },
-        altura: alt,
-        peso: peso,
-        get imc(){
-            const IMC = this.peso / (this.altura * this.altura)
-            return IMC.toFixed(2)
-        }
+// Factory function (Função fábrica)
+// Constructor function (Função construtora)
+function criaPessoa(nome, sobrenome, a, p) {
+  return {
+    nome,
+    sobrenome,
+
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+    },
+
+    fala(assunto = 'falando sobre NADA') {
+      return `${this.nome} está ${assunto}.`;
+    },
+
+    altura: a,
+    peso: p,
+
+    // Getter
+    get imc() {
+      const indice = this.peso / (this.altura ** 2);
+      return indice.toFixed(2);
     }
+  };
 }
-const p1 = criaPessoa('Josias', 'Neto', 1.75, 67)
-p1.nomeCompleto = 'Moisés Figueiredo Santos Machado'
-console.log(p1.nome)
-console.log(p1.sobrenome)
+
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
+const p2 = criaPessoa('João', 'Otávio', 1.90, 57);
+const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
+
